dacb2bdb456fa294407a298bca4de1ba52db2d66
AnaLuizaSena <analuizasena72023@gmail.com>
2026-05-31 19:36:55 -0300

    corrige caminhos errados

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 app.use(express.json({ limit: '50mb' }));
 
 // Servir os arquivos HTML da pasta escola-parte
-app.use(express.static('C:/Users/Lenovo/Downloads/escola-parte'));
+app.use(express.static(path.join(__dirname, 'escola-parte')));
 
 // Conectar ao banco de dados
 const db = new sqlite3.Database(path.join(__dirname, 'mindfine.db'));
